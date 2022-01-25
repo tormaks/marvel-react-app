daeefd9f9c4e89bb1b569e895f3b638757ebd2b0
tormaks <roadtopatience@gmail.com>
2022-01-25 17:32:29 +0300

    feat: create RandomChar component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,13 @@
 import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
 
 const App = () => {
    return (
       <div className="app">
          <AppHeader/>
+         <main className="content">
+            <RandomChar/>
+         </main>
       </div>
    )
 }

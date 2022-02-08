ccaa07b67723ad90b597c21e146a2f36de282da9
Maksim <roadtopatience@gmail.com>
2022-02-08 18:06:14 +0300

    refactor: change RandomChar.js component with hooks

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import {useState, useEffect} from 'react';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -6,75 +6,64 @@ import ErrorMessage from '../errorMessage/ErrorMessage';
 import './randomChar.scss';
 import mjolnir from '../../resources/img/mjolnir.png';
 
-class RandomChar extends Component {
-   state = {
-      char: {},
-      loading: true,
-      error: false,
+const RandomChar = () => {
+   const [char, setChar] = useState({});
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState(false);
+
+   const marvelService = new MarvelService();
+
+   useEffect(() => {
+      updateChar();
+   }, [])
+
+   const onCharLoaded = (char) => {
+      setChar(char);
+      setLoading(false);
+      setError(false);
    }
 
-   marvelService = new MarvelService();
-
-   componentDidMount() {
-      this.updateChar();
+   const onCharLoading = () => {
+      setLoading(true);
    }
 
-   onCharLoaded = (char) => {
-      this.setState({
-         char,
-         loading: false,
-         error: false,
-      });
+   const onError = () => {
+      setLoading(false);
+      setError(true);
    }
 
-   onCharLoading = () => {
-      this.setState({
-         loading: true,
-      })
-   }
-
-   onError = () => {
-      this.setState({
-         loading: false,
-         error: true,
-      });
-   }
-
-   updateChar = () => {
+   const updateChar = () => {
       const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
-      this.onCharLoading();
-      this.marvelService.getCharacter(id)
-         .then(this.onCharLoaded)
-         .catch(this.onError);
+      onCharLoading();
+      marvelService.getCharacter(id)
+         .then(onCharLoaded)
+         .catch(onError);
    }
 
-   render() {
-      const {char, loading, error} = this.state;
-      const spinner = loading ? <Spinner/> : null;
-      const errorMessage = error ? <ErrorMessage/> : null;
-      const content = !(spinner || errorMessage) ? <View char={char}/> : null;
-      return (
-         <section className="randomchar">
-            {spinner}
-            {errorMessage}
-            {content}
-            <div className="randomchar__static">
-               <p className="randomchar__title">
-                  Random character for today!<br/>
-                  Do you want to get to know him better?
-               </p>
-               <p className="randomchar__title">
-                  Or choose another one
-               </p>
-               <button className="button button__main"
-                        onClick={() => this.componentDidMount()}>
-                  <div className="inner">try it</div>
-               </button>
-               <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
-            </div>
-         </section>
-      )
-   }
+   const spinner = loading ? <Spinner/> : null;
+   const errorMessage = error ? <ErrorMessage/> : null;
+   const content = !(spinner || errorMessage) ? <View char={char}/> : null;
+   return (
+      <section className="randomchar">
+         {spinner}
+         {errorMessage}
+         {content}
+         <div className="randomchar__static">
+            <p className="randomchar__title">
+               Random character for today!<br/>
+               Do you want to get to know him better?
+            </p>
+            <p className="randomchar__title">
+               Or choose another one
+            </p>
+            <button className="button button__main"
+                     onClick={updateChar}>
+               <div className="inner">try it</div>
+            </button>
+            <img src={mjolnir} alt="mjolnir" className="randomchar__decoration"/>
+         </div>
+      </section>
+   )
 }
 
 const View = ({char}) => {

31cc457cc7f8df68f2ae247d93545f006d642153
Maksim <roadtopatience@gmail.com>
2022-02-12 12:12:43 +0300

    feat: realization navigation in app

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,38 +1,22 @@
-import {useState} from 'react';
-import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import AppHeader from '../appHeader/AppHeader';
+import {MainPage, ComicsPage} from '../pages';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-import decoration from '../../resources/img/vision.png';
 
 const App = () => {
 
-   const [selectedChar, setChar] = useState(null);
-   
-   const onCharSelected = (id) => {
-      setChar(id);
-   }
-
    return (
-      <div className="app">
-         <AppHeader/>
-         <main className="content">
-            <ErrorBoundary>
-               <RandomChar/>
-            </ErrorBoundary>
-            <div className="char__content">
-               <ErrorBoundary>
-                  <CharList onCharSelected={onCharSelected}/>
-               </ErrorBoundary>
-               <ErrorBoundary>
-                  <CharInfo charId={selectedChar}/>
-               </ErrorBoundary>
-            </div>
-            <img className="bg-decoration" src={decoration} alt="vision" />
-         </main>
-      </div>
+       <Router>
+          <div className="app">
+             <AppHeader/>
+             <main className="content">
+                <Routes>
+                    <Route path='/' element={<MainPage/>}/>
+                    <Route path='/comics' element={<ComicsPage/>}/>
+                </Routes>
+             </main>
+          </div>
+       </Router>
    )
 }
 
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,18 +1,25 @@
+import {NavLink} from 'react-router-dom';
 import './appHeader.scss';
 
 const AppHeader = () => {
    return (
       <header className="app__header">
          <h1 className="app__title">
-               <a href="https://www.google.com/">
+               <NavLink to='/'>
                   <span>Marvel</span> information portal
-               </a>
+               </NavLink>
          </h1>
          <nav className="app__menu">
                <ul>
-                  <li><a href="https://www.google.com/">Characters</a></li>
+                  <li><NavLink
+                      end
+                      style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit'})}
+                      to='/'>Characters</NavLink></li>
                   /
-                  <li><a href="https://www.google.com/">Comics</a></li>
+                  <li><NavLink
+                      end
+                      style={({ isActive }) => ({ color: isActive ? '#9f0013' : 'inherit'})}
+                      to='/comics'>Comics</NavLink></li>
                </ul>
          </nav>
       </header>
